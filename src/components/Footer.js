3230bee947961582bd9a06124a28fdc077272e8d
MetalRocks71 <metalrocks71.79@gmail.com>
2026-01-12 11:09:27 +0100

    vbinylcollection

---
+++ src/components/Footer.js
@@ -0,0 +1,40 @@
+
+import { pageLinks } from "../data"
+import { socialLinks } from "../data"
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <ul className="footer-links">
+                  {pageLinks.map((link) => {
+              return (
+                <il key={link.id} className="footer-link" id="footer-link">
+                  <a href={link.href} className="footer-link">
+                    {link.text}
+                  </a>
+                </il>
+              )
+            })},
+         </ul>
+      <ul className="footer-icons">
+        <ul className="footer-icons">
+                 {socialLinks.map((link) => {
+                   const { id, href, icon } = link
+                   return (
+                     <li key={id}>
+                       <a href={href} target="_blank" rel="noopener noreferrer" className="footer-icon">
+                         <i className={icon}></i>
+                       </a>
+                     </li>
+                   ) })
+                 }
+               </ul>
+               </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  )
+}
+export default Footer
