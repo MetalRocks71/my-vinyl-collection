f77f4e6fb91c536ea39ffcd8484750bad47e4903
MetalRocks71 <metalrocks71.79@gmail.com>
2026-01-14 17:12:59 +0100

    add a collaspable button collection .JS

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,40 +1,55 @@
+import { useState } from 'react'
 import Title from './Title'
 import { collection } from '../data'
 
 const Collection = () => {
+  const [open, setOpen] = useState(false)
+
+  const toggle = () => {
+    setOpen(!open)
+  }
+
   return (
     <section className="section" id="collection">
       <Title title="The " subtitle="Collection" />
-
-      <div className="section-center featured-center">
-        {collection.map((collection) => {
-          const { id, image, title, date, info, band, length, genre } =
-            collection
-          return (
-            <article className="collection-card" key={id}>
-              <div className="collection-img-container">
-                <img src={image} className="collection-img" alt="" />
-                <p className="collection-date">{date}</p>
-              </div>
-              <div className="collection-info">
-                <div className="collection-title">
-                  <h4>{title}</h4>
-                </div>
-                <p>{info}</p>
-                <div className="collection-footer">
-                  <p>
-                    <span>
-                      <i className="fas fa-map"></i>
-                    </span>{' '}
-                    {band}
-                  </p>
-                  <p> {length}</p>
-                  <p>{genre}</p>
-                </div>
-              </div>
-            </article>
-          )
-        })}
+      <div>
+        <button className="category-btn" onClick={toggle}>
+          Heavy Metal
+        </button>
+        {open && (
+          <div>
+            <div className="section-center featured-center">
+              {collection.map((collection) => {
+                const { id, image, title, date, info, band, length, genre } =
+                  collection
+                return (
+                  <article className="collection-card" key={id}>
+                    <div className="collection-img-container">
+                      <img src={image} className="collection-img" alt="" />
+                      <p className="collection-date">{date}</p>
+                    </div>
+                    <div className="collection-info">
+                      <div className="collection-title">
+                        <h4>{title}</h4>
+                      </div>
+                      <p>{info}</p>
+                      <div className="collection-footer">
+                        <p>
+                          <span>
+                            <i className="fas fa-map"></i>
+                          </span>{' '}
+                          {band}
+                        </p>
+                        <p> {length}</p>
+                        <p>{genre}</p>
+                      </div>
+                    </div>
+                  </article>
+                )
+              })}
+            </div>
+          </div>
+        )}
       </div>
     </section>
   )
