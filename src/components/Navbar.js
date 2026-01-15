fed721a29c593be58a56f9f61f8ed323febef919
MetalRocks71 <metalrocks71.79@gmail.com>
2026-01-15 13:48:00 +0100

    added media 767

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,37 @@
 import { pageLinks, socialLinks } from '../data'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false)
+  const [showNavbar, setShowNavbar] = useState(true)
+  const [lastScrollY, setLastScrollY] = useState(0)
+
+  useEffect(() => {
+    const controlNavbar = () => {
+      if (typeof window !== 'undefined') {
+        if (window.scrollY > lastScrollY && window.scrollY > 100) {
+          // scrolling down and past 100px
+          setShowNavbar(false)
+        } else {
+          // scrolling up
+          setShowNavbar(true)
+        }
+        setLastScrollY(window.scrollY)
+      }
+    }
+
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlNavbar)
+
+      return () => {
+        window.removeEventListener('scroll', controlNavbar)
+      }
+    }
+  }, [lastScrollY])
+
   return (
-    <nav className="navbar">
+    <nav
+      className={`navbar ${showNavbar ? 'navbar-visible' : 'navbar-hidden'}`}>
       <div className="nav-center">
         <div className="nav-header">
           <button
@@ -20,7 +47,7 @@ const Navbar = () => {
           <ul className={showLinks ? 'nav-links show-links' : 'nav-links'}></ul>
           {pageLinks.map((link) => {
             return (
-              <li key={link.id} className="nav-links" id="nav-links">
+              <li key={link.id} id="nav-links">
                 <a href={link.href} className="nav-link">
                   {link.text}
                 </a>
