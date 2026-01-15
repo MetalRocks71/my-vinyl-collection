9815b0892cdf1a03568d20e702d78bccfc3e0f88
MetalRocks71 <metalrocks71.79@gmail.com>
2026-01-15 11:16:57 +0100

    added button top component

--- src/components/Tops.js
+++ src/components/Tops.js
@@ -1,12 +1,20 @@
+import { useState } from 'react'
 import Title from './Title'
 import { top } from '../data'
+
 const Tops = () => {
+  const [showAll, setShowAll] = useState(false)
+  const displayedTops = showAll ? top : top.slice(0, 5)
+
   return (
     <section className="section tops" id="top">
       <Title title="Top" subtitle="10 Albums" />
+      <button className="category-btn" onClick={() => setShowAll(!showAll)}>
+        {showAll ? 'Show Less' : 'Show More'}
+      </button>
       <div className="section-center tops-center">
-        {top.map((top) => {
-          const { id, icon, title, text } = top
+        {displayedTops.map((top) => {
+          const { id, icon, title, text, image } = top
           return (
             <article className="top" key={id}>
               <span className="top-icon">
@@ -15,6 +23,7 @@ const Tops = () => {
               <div className="top-info">
                 <h4 className="top-title">{title}</h4>
                 <p className="top-text">{text}</p>
+                <img src={image} alt={title} />
               </div>
             </article>
           )
